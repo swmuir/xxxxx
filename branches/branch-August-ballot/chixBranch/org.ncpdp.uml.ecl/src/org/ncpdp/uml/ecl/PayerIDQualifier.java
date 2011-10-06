57430325fbc8d243f8f704d1298696beb41a7689
tcheng <tcheng@0837804a-1577-0410-aeab-e204489bf47d>
2011-10-06 03:51:52 +0000

    Make May Ballot becomes base since it becomes official in Oct

---
+++ branches/branch-August-ballot/chixBranch/org.ncpdp.uml.ecl/src/org/ncpdp/uml/ecl/PayerIDQualifier.java
@@ -0,0 +1,320 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.ncpdp.uml.ecl;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.emf.common.util.Enumerator;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the literals of the enumeration '<em><b>Payer ID Qualifier</b></em>',
+ * and utility methods for working with them.
+ * <!-- end-user-doc -->
+ * @see org.ncpdp.uml.ecl.ExternalCodeListPackage#getPayerIDQualifier()
+ * @model
+ * @generated
+ */
+public enum PayerIDQualifier implements Enumerator {
+	/**
+	 * The '<em><b>Payer ID Qualifier Blank</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER_BLANK_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	PAYER_ID_QUALIFIER_BLANK(0, "PayerIDQualifierBlank", "PayerIDQualifierBlank"),
+
+	/**
+	 * The '<em><b>Payer ID Qualifier01</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER01_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	PAYER_ID_QUALIFIER01(1, "PayerIDQualifier01", "PayerIDQualifier01"),
+
+	/**
+	 * The '<em><b>Payer ID Qualifier02</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER02_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	PAYER_ID_QUALIFIER02(2, "PayerIDQualifier02", "PayerIDQualifier02"),
+
+	/**
+	 * The '<em><b>Payer ID Qualifier03</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER03_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	PAYER_ID_QUALIFIER03(3, "PayerIDQualifier03", "PayerIDQualifier03"),
+
+	/**
+	 * The '<em><b>Payer ID Qualifier04</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER04_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	PAYER_ID_QUALIFIER04(4, "PayerIDQualifier04", "PayerIDQualifier04"),
+
+	/**
+	 * The '<em><b>Payer ID Qualifier99</b></em>' literal object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER99_VALUE
+	 * @generated
+	 * @ordered
+	 */
+	PAYER_ID_QUALIFIER99(5, "PayerIDQualifier99", "PayerIDQualifier99");
+
+	/**
+	 * The '<em><b>Payer ID Qualifier Blank</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>Payer ID Qualifier Blank</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER_BLANK
+	 * @model name="PayerIDQualifierBlank"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int PAYER_ID_QUALIFIER_BLANK_VALUE = 0;
+
+	/**
+	 * The '<em><b>Payer ID Qualifier01</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>Payer ID Qualifier01</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER01
+	 * @model name="PayerIDQualifier01"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int PAYER_ID_QUALIFIER01_VALUE = 1;
+
+	/**
+	 * The '<em><b>Payer ID Qualifier02</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>Payer ID Qualifier02</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER02
+	 * @model name="PayerIDQualifier02"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int PAYER_ID_QUALIFIER02_VALUE = 2;
+
+	/**
+	 * The '<em><b>Payer ID Qualifier03</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>Payer ID Qualifier03</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER03
+	 * @model name="PayerIDQualifier03"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int PAYER_ID_QUALIFIER03_VALUE = 3;
+
+	/**
+	 * The '<em><b>Payer ID Qualifier04</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>Payer ID Qualifier04</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER04
+	 * @model name="PayerIDQualifier04"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int PAYER_ID_QUALIFIER04_VALUE = 4;
+
+	/**
+	 * The '<em><b>Payer ID Qualifier99</b></em>' literal value.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of '<em><b>Payer ID Qualifier99</b></em>' literal object isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @see #PAYER_ID_QUALIFIER99
+	 * @model name="PayerIDQualifier99"
+	 * @generated
+	 * @ordered
+	 */
+	public static final int PAYER_ID_QUALIFIER99_VALUE = 5;
+
+	/**
+	 * An array of all the '<em><b>Payer ID Qualifier</b></em>' enumerators.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static final PayerIDQualifier[] VALUES_ARRAY =
+		new PayerIDQualifier[] {
+			PAYER_ID_QUALIFIER_BLANK,
+			PAYER_ID_QUALIFIER01,
+			PAYER_ID_QUALIFIER02,
+			PAYER_ID_QUALIFIER03,
+			PAYER_ID_QUALIFIER04,
+			PAYER_ID_QUALIFIER99,
+		};
+
+	/**
+	 * A public read-only list of all the '<em><b>Payer ID Qualifier</b></em>' enumerators.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final List<PayerIDQualifier> VALUES = Collections.unmodifiableList(Arrays.asList(VALUES_ARRAY));
+
+	/**
+	 * Returns the '<em><b>Payer ID Qualifier</b></em>' literal with the specified literal value.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static PayerIDQualifier get(String literal) {
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			PayerIDQualifier result = VALUES_ARRAY[i];
+			if (result.toString().equals(literal)) {
+				return result;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the '<em><b>Payer ID Qualifier</b></em>' literal with the specified name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static PayerIDQualifier getByName(String name) {
+		for (int i = 0; i < VALUES_ARRAY.length; ++i) {
+			PayerIDQualifier result = VALUES_ARRAY[i];
+			if (result.getName().equals(name)) {
+				return result;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the '<em><b>Payer ID Qualifier</b></em>' literal with the specified integer value.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static PayerIDQualifier get(int value) {
+		switch (value) {
+			case PAYER_ID_QUALIFIER_BLANK_VALUE: return PAYER_ID_QUALIFIER_BLANK;
+			case PAYER_ID_QUALIFIER01_VALUE: return PAYER_ID_QUALIFIER01;
+			case PAYER_ID_QUALIFIER02_VALUE: return PAYER_ID_QUALIFIER02;
+			case PAYER_ID_QUALIFIER03_VALUE: return PAYER_ID_QUALIFIER03;
+			case PAYER_ID_QUALIFIER04_VALUE: return PAYER_ID_QUALIFIER04;
+			case PAYER_ID_QUALIFIER99_VALUE: return PAYER_ID_QUALIFIER99;
+		}
+		return null;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private final int value;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private final String name;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private final String literal;
+
+	/**
+	 * Only this class can construct instances.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	private PayerIDQualifier(int value, String name, String literal) {
+		this.value = value;
+		this.name = name;
+		this.literal = literal;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public int getValue() {
+	  return value;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getName() {
+	  return name;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getLiteral() {
+	  return literal;
+	}
+
+	/**
+	 * Returns the literal value of the enumerator, which is its string representation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		return literal;
+	}
+	
+} //PayerIDQualifier
